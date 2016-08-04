7d2b5ce5d505ff9e0d67b7ed101040f3df334406
Shruti Rao <shruti.rao@feisystems.com>
2016-08-04 11:47:52 -0400

    Throw custom exception on DataIntegrityViolationException
    # 9971:C2S v2 :: Admin Portal :: Patient Creation :: Show custom error message when a patient is created with an email Id that's already used.

---
+++ phr/src/main/java/gov/samhsa/mhc/phr/service/exception/PatientNotSavedException.java
@@ -0,0 +1,26 @@
+package gov.samhsa.mhc.phr.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class PatientNotSavedException extends RuntimeException {
+    public PatientNotSavedException() {
+    }
+
+    public PatientNotSavedException(String message) {
+        super(message);
+    }
+
+    public PatientNotSavedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PatientNotSavedException(Throwable cause) {
+        super(cause);
+    }
+
+    public PatientNotSavedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
